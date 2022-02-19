09c1235372ab515e543537a5a10284b94f6dfb51
Janne Immonen <immonenjanne@icloud.com>
2022-02-20 01:44:19 +0200

    Initial Commit

---
+++ KorttiPeli/KorttiPeliApp.swift
@@ -0,0 +1,17 @@
+//
+//  KorttiPeliApp.swift
+//  KorttiPeli
+//
+//  Created by Janne Immonen on 26.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
